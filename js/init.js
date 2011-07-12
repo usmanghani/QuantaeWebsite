31769ade49bc01d7b00277a9a0d31fd8429e0b37
Usman Ghani <mugga@monaco.dreamhost.com>
2011-07-12 00:17:24 -0700

    First commit to website without flash videos. They will come in a separate commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#slidertron').slidertron({
+		viewerSelector:      '.viewer',
+		reelSelector:        '.viewer .reel',
+		slidesSelector:      '.viewer .reel .slide',
+		navPreviousSelector: '.prev',
+		navNextSelector:     '.next'
+	});
+});
